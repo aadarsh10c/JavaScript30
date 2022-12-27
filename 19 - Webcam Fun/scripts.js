d08152b9ccc4406a42d66ceb515a9d4f833e347b
Adarsh Kumar <adarsh010c@gmail.com>
2022-12-27 20:08:45 +0530

    Programmed to get access for mediastream of webcam

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,21 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+//get video
+async function getVideo(){
+    let localStream = null;
+    console.log( 'inside function');
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({video : false , audio : false});
+
+        console.log( localStream );
+    } catch (error) {
+        
+    }
+
+    
+}
+
+getVideo();
